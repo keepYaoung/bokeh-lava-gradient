91aa051459347d0b66acf4bff068f2612e19474e
Tommy <vbdd556@gmail.com>
2026-06-16 16:46:47 +0900

    Publish bg-only live (light2Mask no scrim) [skip ci]

--- docs/bg-only/flutter_bootstrap.js
+++ docs/bg-only/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3040050932" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1868751944" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
